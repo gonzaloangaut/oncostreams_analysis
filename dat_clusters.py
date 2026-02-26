7339b13c134337f35b882a96a6135234abdf18a1
gonzaloangaut <angaut13@gmail.com>
2026-02-26 11:33:48 -0300

    Added more data for N=10.000

--- dat_clusters.py
+++ dat_clusters.py
@@ -216,8 +216,8 @@ def save_distribution_last_step_no_giant(num_cells, dens, rng_seed, last_step):
 
 
 # Parameters
-density_list = [0.75, 0.76]
-nc = 5_000
+density_list = [0.77, 0.78, 0.79]
+nc = 10_000
 max_step = 80_000
 step = 100
 number_of_realizations = 64
--- data/N=10_000/dat_final.py
+++ data/N=10_000/dat_final.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 # Suponiendo que ya tienes estas variables definidas
-list_density = [0.76]  # ejemplo de densidades
+list_density = [0.77, 0.78, 0.79]  # ejemplo de densidades
 num_cells = 10_000  # número de células
 max_step = 80_000  # paso máximo
 step = 100  # intervalo de pasos
