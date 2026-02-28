eea0514ad1cc9c8910a41b3ce412b677fd52aab0
gonzaloangaut <angaut13@gmail.com>
2026-02-28 09:26:36 -0300

    Added data for N=3.000

--- data/N=3_000/dat_final.py
+++ data/N=3_000/dat_final.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 # Suponiendo que ya tienes estas variables definidas
-list_density = [0.76, 0.77, 0.78, 0.79]  # ejemplo de densidades
+list_density = [0.86]  # ejemplo de densidades
 num_cells = 3_000  # número de células
 max_step = 80_000  # paso máximo
 step = 100  # intervalo de pasos
