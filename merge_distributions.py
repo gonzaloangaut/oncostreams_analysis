e08685bf6f6675267ad2a2b6cc41d0344fe3e503
gonzaloangaut <angaut13@gmail.com>
2026-04-29 13:31:01 -0300

    Analyzed phenotypes for new data for N=2.000

--- merge_distributions.py
+++ merge_distributions.py
@@ -61,10 +61,10 @@ def merge_all_distributions_for_N(n_cells, densities):
 # Execute
 if __name__ == "__main__":
     # Define the densities you want to process for N=1000
-    n_cells_target = 1000
+    n_cells_target = 2000
     densities_target = [
         0.4, 0.5, 0.6, 0.7, 0.75, 0.76, 0.77, 0.78, 0.79, 0.8,
-        0.81, 0.82, 0.83, 0.84, 0.843, 0.845, 0.848, 0.85, 0.852,
+        0.81, 0.82, 0.83, 0.84, 0.845, 0.848, 0.85, 0.852,
         0.855, 0.86, 0.87, 0.88, 0.9 
     ]
 
